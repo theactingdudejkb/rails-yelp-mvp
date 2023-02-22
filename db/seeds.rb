703923ff9f3b2e23ed602b5cb3cce2f43692ed1f
James Bunyon <bunyonjames@gmail.com>
2023-02-22 17:00:45 +0100

    linking form

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.destory_all
+Restaurant.create(name: 'Burger King', address: 'Cologne', phone_number: '0220 565856', category: 'french')
+Restaurant.create(name: 'Haus des Donas', address: 'Dortmund', phone_number: '0220 56728232', category: 'japanese')
+Restaurant.create(name: 'KFC', address: 'Bonn', phone_number: '0212 537656', category: 'belgian')
+Restaurant.create(name: 'Thai House', address: 'Cologne', phone_number: '0220 529651', category: 'italian')
+Restaurant.create(name: 'Mc Donalds', address: 'Berlin', phone_number: '0100 567698',  category: 'chinese')
